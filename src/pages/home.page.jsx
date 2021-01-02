36518f6ddef37a7c3b5bbc692f9fc2a9204939f6
test <test@gmail.com>
2021-01-03 01:57:42 +0330

    e

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,5 +1,15 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import { ReactComponent as FullCycle } from "./../assets/full-cycle.svg";
+import { ReactComponent as CustomApp } from "./../assets/custom-app.svg";
+import { ReactComponent as ReadyToUse } from "./../assets/readyToUse.svg";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+import Team from "./../assets/ball@2x.cae3730.png";
+import Tech from "./../assets/gear️@2x.815757c.png";
 
 const useStyles = makeStyles((theme) => ({
   ///////////////////////////////// main
@@ -10,12 +20,29 @@ const useStyles = makeStyles((theme) => ({
     overflow: "hidden",
     maxWidth: "100vw",
   },
+  container: {
+    width: "100%",
+    maxWidth: 1152,
+    padding: "0 16px",
+    boxSizing: "border-box",
+    margin: "0 auto",
+    height: "100%",
+  },
+  MainRow: {
+    display: "flex",
+    position: "relative",
+    height: "100%",
+
 }));
 
 const HomePage = () => {
   const classes = useStyles();
 
-  return <Fragment>home</Fragment>;
+  return (
+    <Fragment>
+      home
+    </Fragment>
+  );
 };
 
 export default HomePage;
