8d7aa24e814053efbe6cf68a689c8ed29abe508b
test <test@gmail.com>
2021-01-04 01:55:13 +0330

    e

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -45,11 +45,11 @@ export function* signInWithEmail({ payload: { email, password } }) {
         password,
       },
     }).then((response) => response.data);
-    // if (user.status === "success") {
-    //   return window.setTimeout(() => {
-    //     location.assign("/admin");
-    //   }, 1500);
-    // }
+    if (user.status === "success") {
+      return window.setTimeout(() => {
+        location.assign("/");
+      }, 1500);
+    }
     yield put(signInSuccess(user));
   } catch (error) {
     yield put(signInFailure(error));
