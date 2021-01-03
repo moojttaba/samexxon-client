aa10d59adff0eeae2bf106c9d45fbb4588896d09
test <test@gmail.com>
2021-01-03 11:16:27 +0330

    e

--- src/containers/drawer-cart-container.jsx
+++ src/containers/drawer-cart-container.jsx
@@ -26,7 +26,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
-const drawerWidth = 140;
+const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   drawer: {
     backgroundColor: theme.palette.primary.main,
