93ffffaa43bdf8eab8ec4ed5a4475b7fc75ce50d
test <test@gmail.com>
2021-01-03 16:53:58 +0330

    e

--- src/pages/about-us.page.jsx
+++ src/pages/about-us.page.jsx
@@ -6,30 +6,15 @@ import ImageGridList from "../containers/project-list.container";
 
 const useStyles = makeStyles((theme) => ({
   ///////////////////////////////// main
-  homeMain: {
-    minHeight: 630,
-    height: "calc(100vh - 100px)",
-    padding: "143px 0 40px",
-    overflow: "hidden",
-    maxWidth: "100vw",
-  },
-  container: {
-    width: "100%",
-    maxWidth: 1152,
-    padding: "0 16px",
-    boxSizing: "border-box",
-    margin: "0 auto",
-    height: "100%",
-  },
- 
+  homeMain: {},
 }));
 
 const HomePage = () => {
-  //const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <Fragment>
-      <Container>
+      <Container className={classes.homeMain}>
         <ImageGridList />
       </Container>
     </Fragment>
--- src/pages/contact.page.jsx
+++ src/pages/contact.page.jsx
@@ -6,28 +6,18 @@ import ContactList from "./../containers/contact-list.container";
 
 const useStyles = makeStyles((theme) => ({
   homeMain: {
-    minHeight: 630,
-    height: "calc(100vh - 100px)",
-    padding: "143px 0 40px",
-    overflow: "hidden",
-    maxWidth: "100vw",
+
   },
-  container: {
-    width: "100%",
-    maxWidth: 1152,
-    padding: "0 16px",
-    boxSizing: "border-box",
-    margin: "0 auto",
-    height: "100%",
-  },
+ 
+
 }));
 
 const ContactPage = () => {
-  //const classes = useStyles();
+ const classes = useStyles();
 
   return (
     <Fragment>
-      <Container>
+      <Container className={classes.homeMain}>
         <ContactList />
       </Container>
     </Fragment>
