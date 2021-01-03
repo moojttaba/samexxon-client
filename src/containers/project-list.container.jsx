355774d2bd411f75b6d2737f96a8d0eb0245ab02
test <test@gmail.com>
2021-01-03 21:11:28 +0330

    ث

--- src/containers/project-list.container.jsx
+++ src/containers/project-list.container.jsx
@@ -95,9 +95,7 @@ const ImageGridList = ({ tileData }) => {
                     color="textSecondary"
                     component="p"
                   >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
+                    {tile.author}
                   </Typography>
                 </CardContent>
               </CardActionArea>
